efd9069eb4cbd9bd00636d426a57a1168a8ca4a0
Abhie9 <patelabhi9718@gmail.com>
2021-06-09 06:56:13 -0400

    Extended API for App user Role Management

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,12 +1,11 @@
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,72 +13,73 @@ namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager,
+        SignInManager<AppUser> signInManager,
+         ITokenService tokenService, IMapper mapper)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
             _mapper = mapper;
             _tokenService = tokenService;
-            _context = context;
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+
+        if (await userExists(registerDto.Username)) return BadRequest("Username is already taken!");
+
+        var user = _mapper.Map<AppUser>(registerDto);
+
+        user.UserName = registerDto.Username.ToLower();
+
+        var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+        if(!result.Succeeded)   return BadRequest(result.Errors);
+
+        var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+        if(!roleResult.Succeeded) return BadRequest(result.Errors);
+
+        return new UserDto
         {
-
-            if (await userExists(registerDto.Username)) return BadRequest("Username is already taken!");
-
-            var user = _mapper.Map<AppUser>(registerDto);
-            using var hmac = new HMACSHA512();
-            
-                user.UserName = registerDto.Username.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-                user.PasswordSalt = hmac.Key;
-
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
-                knownAs = user.KnownAs,
-                Gender = user.Gender
-            };
-        }
-
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-        {
-            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
-
-            if (user == null) return Unauthorized("Invalid Credentials!");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Credentials!");
-            }
-
-            return new UserDto
-            {
-                Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
-                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                knownAs = user.KnownAs,
-                Gender = user.Gender
-
-            };
-        }
-
-
-        private async Task<bool> userExists(string username)
-        {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
-        }
+            Username = user.UserName,
+            Token = await _tokenService.CreateToken(user),
+            knownAs = user.KnownAs,
+            Gender = user.Gender
+        };
     }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _userManager.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid Credentials!");
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if(!result.Succeeded) return Unauthorized();
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = await _tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            knownAs = user.KnownAs,
+            Gender = user.Gender
+
+        };
+    }
+
+
+    private async Task<bool> userExists(string username)
+    {
+        return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
+    }
+}
 }
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -11,22 +10,37 @@ namespace API.Data
     //add data into database from json file
     public class Seed
     {
-        public static async Task SeedUser(DataContext context){
-            if(await context.Users.AnyAsync()) return;
+        public static async Task SeedUser(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager){
+            if(await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if(users == null) return;
+
+            var roles = new List<AppRole>{
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
-
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Mingle$9"));
-                user.PasswordSalt = hmac.Key;
-
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "Mingle$9");
+                await userManager.AddToRoleAsync(user, "Member");
             }
-            await context.SaveChangesAsync();
+
+            var admin = new AppUser{
+                UserName = "abhie"
+            };
+
+            await userManager.CreateAsync(admin, "Mingle$9");
+            await userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
         }
     }
 }
